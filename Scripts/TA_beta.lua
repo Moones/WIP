2d5bfd71bcc6fb817b6826c3da264fb56bb2494a
Alois Pospíšil <moones@email.cz>
2014-09-10 16:14:18 +0200

    Update TA_beta.lua

--- Scripts/TA_beta.lua
+++ Scripts/TA_beta.lua
@@ -173,7 +173,7 @@ function OrbWalk(me)
 	--find out if it is illusion or not
 	for i=1,#enemies do
 		if enemies[i]:IsIllusion() then
-			enemies[i] = enemies[i+1]
+			enemies[i] = nil
 		end
 	end	
 	--if we got more enemies around and victim is far choose lowest HP target instead
