7c7bc8253fb0a2fedd7894c687ddeab4780228dd
Alois Pospíšil <moones@email.cz>
2014-09-17 00:59:52 +0200

    Update GetKeyCode.lua

--- Scripts/GetKeyCode.lua
+++ Scripts/GetKeyCode.lua
@@ -1,7 +1,10 @@
-require("libs.Utils")
-
 function Key(msg, code)
-	print(msg, code)
+	if msg and code then
+		print(msg, code)
+	elseif msg then
+		print(msg)
+	elseif code then
+		print(code)
+	end
 end
-
 script:RegisterEvent(EVENT_KEY, Key)
