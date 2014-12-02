1f775a9edbe630b6ec77102a23d06d295f1c5ee7
Moon Esconic <moones@email.cz>
2014-12-02 16:57:14 +0100

    Update SpellDamage.lua

--- Libraries/SpellDamage.lua
+++ Libraries/SpellDamage.lua
@@ -41,8 +41,15 @@ SpellDamage.modifiersSpellList = {
 
 SpellDamage.attackModifiersList = {
 	antimage_mana_break = { damage = "mana_per_hit"; multiplier = 0.6; };
-	venomancer_poison_sting = { tickDamage = "damage"; tickDuration = "duration"; tickInterval = 1; starTime = 0; };
-	viper_poison_attack = { tickDamage = "damage"; tickDuration = "duration"; tickInterval = 1; starTime = 1; };
+	venomancer_poison_sting = { tickDamage = "damage"; tickDuration = "duration"; tickInterval = 1; startTime = 0; };
+	viper_poison_attack = { tickDamage = "damage"; tickDuration = "duration"; tickInterval = 1; startTime = 1; };
+	clinkz_searing_arrows = { damage = "damage_bonus"; };
+	enchantress_impetus = { distance_as_damage = "distance_damage_pct"; };
+	huskar_burning_spear = { tickDuration = "8"; };
+	bounty_hunter_jinada = { multiplier = "crit_multiplier"; cooldown = true; };
+	weaver_geminate_attack = { multiplier = 2; cooldown = true; };
+	jakiro_liquid_fire = { tickDamage = "damage"; tickInterval = 1; startTime = 0.5; tickDuration = 5; };
+	spectre_desolate = { damage = "bonus_damage"; special = true; };
 }
 
 --Spells not listed here returns damage from LuaEntityAbility:GetDamage()
@@ -81,13 +88,41 @@ SpellDamage.spellList = {
 	enigma_malefice = { damage = "damage"; multiplier = 3; };
 	necrolyte_reapers_scythe = { damage = "damage_per_health"; damageScepter = "damage_per_health_scepter"; };
 	warlock_shadow_word = { tickDuration = "duration"; tickInterval = "tick_interval"; startTime = 1; };
-	beastmaster_primal_roar = { damage = "damage"; }
+	beastmaster_primal_roar = { damage = "damage"; };
 	queenofpain_shadow_strike = { damage = "strike_damage"; tickDamage = "duration_damage"; tickInterval = 3; startTime = 3; tickDuration = 15; };
 	queenofpain_sonic_wave = { damage = "damage"; damageScepter = "damage_scepter"; };
 	venomancer_venomous_gale = { damage = "strike_damage"; tickDuration = "duration"; tickDamage = "tick_damage"; tickInterval = "tick_interval"; startTime = 3; };
 	venomancer_poison_nova = { tickDamage = "damage"; tickDuration = "duration"; tickDamageScepter = "damage_scepter"; tickDurationScepter = "duration_scepter"; tickInterval = 1; startTime = 0; };
-	templar_assassin_meld = { damage = "bonus_damage"; }
-	viper_viper_strike = { tickDamage = "damage"; tickDuration = "duration"; tickInterval = 1; starTime = 1; }
+	templar_assassin_meld = { damage = "bonus_damage"; };
+	viper_viper_strike = { tickDamage = "damage"; tickDuration = "duration"; tickInterval = 1; startTime = 1; };
 	luna_eclipse = { multiplier = "hit_count"; multiplierScepter = "hit_count_scepter"; };
-	
+	dazzle_poison_touch = { startTime = "set_time"; tickInterval = 1; tickDuration = 10; };
+	rattletrap_battery_assault = { tickDuration = "duration"; tickInterval = "interval"; startTime = 0; };
+	rattletrap_hookshot = { damage = "damage"; };
+	leshrac_diabolic_edict = { tickDuration = 8; tickInterval = 0.25; startTime = 0; };
+	leshrac_pulse_nova = { tickDamage = "damage"; tickDamageScepter = "damage_scepter"; tickInterval = 1; startTime = 0; };
+	furion_wrath_of_nature = { damage = "damage"; damageScepter = "damage_scepter"; };
+	life_stealer_infest = { damage = "damage"; };
+	dark_seer_vacuum = { damage = "damage"; };
+	dark_seer_ion_shell = { tickDamage = "damage_per_second"; tickDuration = "duration"; tickInterval = 1; startTime = 0.1; };
+	dark_seer_wall_of_replica = { damage = "damage"; };
+	omniknight_purification = { damage = "heal"; };
+	huskar_life_break = { damage = "health_damage"; damageScepter = "health_damage_scepter"; };
+	broodmother_spawn_spiderlings = { damage = "damage"; };
+	bounty_hunter_shuriken_toss = { damage = "bonus_damage"; };
+	bounty_hunter_wind_walk = { damage = "bonus_damage"; };
+	weaver_shukuchi = { damage = "damage"; };
+	jakiro_dual_breath = { tickDamage = "burn_damage"; tickDuration = "tooltip_duration"; tickInterval = 0.5; startTime = 0.5; tickDuration = 5; };
+	jakiro_ice_path = { damage = "damage"; };
+	jakiro_macropyre = { tickDamage = "damage"; tickDuration = "duration"; tickInterval = 1; tickDamageScepter = "damage_scepter"; tickDurationScepter = "duration_scepter"; startTime = 0.5; };
+	batrider_flamebreak = { damage = "damage"; };
+	batrider_sticky_napalm = { damage = "damage"; };
+	chen_test_of_faith = { maxDamage = "damage_max"; minDamage = "damage_min"; };
+	spectre_spectral_dagger = { damage = "damage"; };
+	doom_bringer_scorched_earth = { tickDamage = "damage_per_second"; tickDuration = "duration";  tickInterval = 1; startTime = 1; };
+	doom_bringer_lvl_death = { damage = "damage"; bonusDamage = "lvl_bonus_damage"; bonusMultiplier = "lvl_bonus_multiple"; };
+	doom_bringer_doom = { tickDamage = "damage"; tickDuration = "duration"; tickDamageScepter = "damage_scepter"; tickDurationScepter = "duration_scepter"; tickInterval = 1; startTime = 0; };
+	ancient_apparition_cold_feet = { tickDamage = "damage"; tickInterval = {0.8,0.8,0.9,0.9}; startTime = 0.8; };
+	--ancient_apparition_ice_blast = { 
+	--too hard to finish
 }
